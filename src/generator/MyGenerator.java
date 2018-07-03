ebea6400e78ffc52064a604f2d9c10a2e9551493
Fabian <Fabian@Fabian-PC>
2018-07-03 12:34:08 +0200

    generating factor almost complete

--- src/generator/MyGenerator.java
+++ src/generator/MyGenerator.java
@@ -1,17 +1,19 @@
 package generator;
 
-import grammar.MyGrammarBaseListener;
-import grammar.MyGrammarParser;
-import grammar.MyGrammarParser.BoolFactorContext;
-import grammar.MyGrammarParser.FactorContext;
-import grammar.MyGrammarParser.NumFactorContext;
-import grammar.MyGrammarParser.ProgramContext;
-import grammar.MyGrammarParser.StatementContext;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.antlr.v4.runtime.tree.ParseTreeProperty;
+
+import grammar.MyGrammarBaseListener;
+import grammar.MyGrammarParser;
+import grammar.MyGrammarParser.ArrayFactorContext;
+import grammar.MyGrammarParser.BoolFactorContext;
+import grammar.MyGrammarParser.CharFactorContext;
+import grammar.MyGrammarParser.NumFactorContext;
+import grammar.MyGrammarParser.ProgramContext;
+import grammar.MyGrammarParser.StatementContext;
+import grammar.MyGrammarParser.VarFactorContext;
 
 public class MyGenerator extends MyGrammarBaseListener {
 
@@ -64,6 +66,24 @@ public class MyGenerator extends MyGrammarBaseListener {
     public void exitBoolFactor(BoolFactorContext ctx) {
     	ArrayList<String> cmds = new ArrayList<>();
     	cmds.add("Load (ImmValue " + (Boolean.parseBoolean(ctx.BOOL().getText())?"1":"0") + ") regA");
+    	cmds.add("Push regA");
+    	commands.put(ctx, cmds);
+    }
+    
+    public void exitVarFactor(VarFactorContext ctx) {
+    	ArrayList<String> cmds = new ArrayList<>();
+    	cmds.add("Load (DirAddr " + variables.get(ctx.ID().getText()) + ") regA");
+    	cmds.add("Push regA");
+    	commands.put(ctx, cmds);
+    }
+    
+    public void exitArrayFactor(ArrayFactorContext ctx) {
+    	// TODO
+    }
+    
+    public void exitCharFactor(CharFactorContext ctx) {
+    	ArrayList<String> cmds = new ArrayList<>();
+    	cmds.add("Load (ImmValue " + Character.getNumericValue(ctx.CHAR().getText().toCharArray()[1]) + ") regA");
     	cmds.add("Push regA");
     	commands.put(ctx, cmds);
     }
