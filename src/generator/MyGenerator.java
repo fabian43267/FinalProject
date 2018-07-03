1c467a16dd5fde2e290d124cc6afa362d3185cb5
Fabian <Fabian@Fabian-PC>
2018-07-03 11:54:19 +0200

    Generator stuff

--- src/generator/MyGenerator.java
+++ src/generator/MyGenerator.java
@@ -2,15 +2,53 @@ package generator;
 
 import grammar.MyGrammarBaseListener;
 import grammar.MyGrammarParser;
+import grammar.MyGrammarParser.BoolFactorContext;
+import grammar.MyGrammarParser.FactorContext;
+import grammar.MyGrammarParser.NumFactorContext;
+import grammar.MyGrammarParser.ProgramContext;
+import grammar.MyGrammarParser.StatementContext;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.antlr.v4.runtime.tree.ParseTreeProperty;
+
 public class MyGenerator extends MyGrammarBaseListener {
 
-    private ArrayList commands = new ArrayList();
-
-    @Override public void enterProgram(MyGrammarParser.ProgramContext ctx) { }
-        
+    private ParseTreeProperty<ArrayList<String>> commands = new ParseTreeProperty<>();
+    
+    // returns a haskell file as a string, given the commands it has generated before
+    /*
+    public String buildHaskellFile() {
+    	String s = "import Sprockell\nprog = [";
+    	for (int i = 0; i < commands..size()-1; i++) {
+    		s += commands.get(i) + ",\n";
+    	}
+    	s += commands.get(commands.size()-1) + "]\n\nmain = run [prog]\n";
+    	return s;
+    }
+    */
+    
+    public void exitProgram(ProgramContext ctx) {
+    	ArrayList<String> cmds = new ArrayList<>();
+    	for (StatementContext bla : ctx.statement()) {
+    		cmds.addAll(commands.get(bla));
+    	}
+    	cmds.add("EndProg");
+    	commands.put(ctx, cmds);
+    }
+    
+    public void exitNumFactor(NumFactorContext ctx) {
+    	ArrayList<String> cmds = new ArrayList<>();
+    	cmds.add("Load (ImmValue " + ctx.NUM().getText() + ") regA");
+    	cmds.add("Push regA");
+    	commands.put(ctx,cmds);
+    }
+    
+    public void exitBoolFactor(BoolFactorContext ctx) {
+    	ArrayList<String> cmds = new ArrayList<>();
+    	cmds.add("Load (ImmValue " + (Boolean.parseBoolean(ctx.BOOL().getText())?"1":"0") + ") regA");
+    	cmds.add("Push regA");
+    }
 
 }
