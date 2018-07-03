9189ef73f85a721a7a226ca391f165d55fe70a16
Radu <r.seteanu@student.utwente.nl>
2018-07-03 11:58:36 +0200

    Merged two versions of the generator

--- src/generator/MyGenerator.java
+++ src/generator/MyGenerator.java
@@ -9,14 +9,31 @@ import grammar.MyGrammarParser.ProgramContext;
 import grammar.MyGrammarParser.StatementContext;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 import org.antlr.v4.runtime.tree.ParseTreeProperty;
 
 public class MyGenerator extends MyGrammarBaseListener {
 
     private ParseTreeProperty<ArrayList<String>> commands = new ParseTreeProperty<>();
-    
+    private HashMap<String, Integer> variables = new HashMap<>();
+    private int r1 = 1, r2 = 2;
+    private int addrTop = 0;
+
+    @Override
+    public void enterProgram(MyGrammarParser.ProgramContext ctx) { }
+
+    @Override
+    public void enterDeclAssign(MyGrammarParser.DeclAssignContext ctx) {
+        ArrayList<String> cmds = new ArrayList<>();
+        variables.put(ctx.ID().getText(), addrTop); // add the variable to the map
+        cmds.add("Load (ImmValue " + ctx.expr() + ") " + r1);
+        cmds.add("Store " + r1 + " (DirAddr " + addrTop + ")");
+        addrTop += 4; //increment the address to make room for the next variable
+        commands.put(ctx, cmds);
+    }
+
+
     // returns a haskell file as a string, given the commands it has generated before
     /*
     public String buildHaskellFile() {
@@ -28,7 +45,7 @@ public class MyGenerator extends MyGrammarBaseListener {
     	return s;
     }
     */
-    
+
     public void exitProgram(ProgramContext ctx) {
     	ArrayList<String> cmds = new ArrayList<>();
     	for (StatementContext bla : ctx.statement()) {
@@ -37,14 +54,14 @@ public class MyGenerator extends MyGrammarBaseListener {
     	cmds.add("EndProg");
     	commands.put(ctx, cmds);
     }
-    
+
     public void exitNumFactor(NumFactorContext ctx) {
     	ArrayList<String> cmds = new ArrayList<>();
     	cmds.add("Load (ImmValue " + ctx.NUM().getText() + ") regA");
     	cmds.add("Push regA");
     	commands.put(ctx,cmds);
     }
-    
+
     public void exitBoolFactor(BoolFactorContext ctx) {
     	ArrayList<String> cmds = new ArrayList<>();
     	cmds.add("Load (ImmValue " + (Boolean.parseBoolean(ctx.BOOL().getText())?"1":"0") + ") regA");
